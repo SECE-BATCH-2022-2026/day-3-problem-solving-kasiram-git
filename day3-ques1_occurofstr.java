4a3fe66ab0aaf820c6d99d4f655319b914f6b5b8
kasiram-git <149467839+kasiram-git@users.noreply.github.com>
2025-12-07 00:38:42 +0530

    updated

--- day3-ques1_occurofstr.java
+++ day3-ques1_occurofstr.java
@@ -13,25 +13,29 @@ public class Main{
 	}
 }
 
-class UserMainCode{
-	public static int countOccurances(String s1, String s2){
-	int count=0;
-	int len1=s1.length();
-	int len2=s2.length();
-	if(len1==0 || len2==0){
-		return 0;
-		}
-		
-	for(int i=0;i<len1;i++){
-		for(int j=0;j<len2;j++){
-			if(s1.charAt(i)==s2.charAt(j)){
-				if(j==len2-1){
-					
-				count++;
-				}
-			}
-		}
-	}
-	return count;
-  }
+
+class UserMainCode {
+    public static int countOccurances(String s1, String s2) {
+        int count = 0;
+
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        if(len1 == 0 || len2 == 0) return 0;
+
+        for(int i = 0; i <= len1 - len2; i++) {
+            // Check substring match
+            int j;
+            for(j = 0; j < len2; j++) {
+                if(s1.charAt(i + j) != s2.charAt(j)) {
+                    break;
+                }
+            }
+            if(j == len2) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
