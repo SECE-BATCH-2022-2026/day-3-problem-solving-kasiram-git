d9082972ba9e50c36077efa323c644f87ca9ed50
kasiram-git <kasiram2k@gmail.com>
2025-12-06 23:46:09 +0530

    Final task Completed!

---
+++ day3-ques3_findtriplets.java
@@ -0,0 +1,29 @@
+// 3.checks whether the input array contains any 3 consecutive identical numbers [or] findTriplets
+
+import java.util.*;
+class Main{
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		
+		int arr[]=new int[n];
+		
+		for(int i=0;i<n;i++){
+			arr[i]=scan.nextInt();
+		}
+		
+		System.out.print(UserMainCode.findTriplet(arr));
+		
+	}
+}
+
+class UserMainCode{
+	   public static boolean findTriplet(int[] nums){
+	   	for(int i=0;i<nums.length-2;i++){
+	   		if(nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
+	   			return true;
+	   		}
+	   	}
+	   	return false;
+	   } 
+}
