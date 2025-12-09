ecdf92f194edc740038c24accf1f98dc396c4611
kasiram-git <149467839+kasiram-git@users.noreply.github.com>
2025-12-09 16:02:50 +0530

    Problem 4 (Finished) - Repeat the First 3 chars n time
    Day2 -- Problem 1

--- repeatFront.java
+++ repeatFront.java
@@ -1,4 +1,6 @@
 // 1.Repeat Front string and number will be provided by user the program must print the first three characters of the string as n times and if the string length is less than 3 the string took as front and print n times. //eg. Mukesh Kumar B, 3 -> Muk Muk Muk
+// Day2 -- Problem 1
+
 import java.util.*;
 
 class repeatFront{
