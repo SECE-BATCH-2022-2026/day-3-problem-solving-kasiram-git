63a5c54e1fbded77033f0d2b953afd71ccf549f4
kasiram-git <149467839+kasiram-git@users.noreply.github.com>
2025-12-13 15:02:06 +0530

    Problem 9 (Finished) - Pattern Matching2
    Day4 -- Problem2

--- patternChecker2.java
+++ patternChecker2.java
@@ -3,7 +3,7 @@
 
 import java.util.*;
 
-class PatternChecker2{
+class patternChecker2{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		String ip = sc.nextLine();
@@ -12,12 +12,10 @@ class PatternChecker2{
 }
 
 class UserMainCode {
-	public static boolean regularExpression(String ip){
-		
+	public static boolean regularExpression(String ip){	
 		/*Pattern p = Patten.conpile("[a-zA-Z0-9][R][0-9][a-zA-Z0-9]");
 		Matcher m = p.matcher(ip);
 		return m.matches();*/
-		
 		return ip.matches("(^[2][1-9]$)|(^[3][0-9]$)|(^[4][0-5]$)");
 	}
 }
